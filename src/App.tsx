a36f3ea5fda7039e907b3aee7f207e75fa85ee36
Hugo Sanches <74740073+hugocsdv@users.noreply.github.com>
2025-04-19 15:07:39 -0300

    remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import TesouroIndividual from './TesouroIndividual'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
